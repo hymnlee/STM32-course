18e7fa58ff61bbdbbfa6eae647b41d90c54f064f
oraSC <1301912993@qq.com>
2019-03-31 15:31:57 +0800

    上传Template

---
+++ 实验0 Template工程模板/USER/main.c
@@ -0,0 +1,13 @@
+#include "stm32f4xx.h"
+
+
+int main(void)
+{
+	/*********************************************/
+	RCC_ClocksTypeDef RCC_ClocksStructure;
+	
+	RCC_GetClocksFreq(&RCC_ClocksStructure);
+	
+	/***********************************************/
+
+}
